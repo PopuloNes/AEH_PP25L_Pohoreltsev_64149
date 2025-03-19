6592adf8ff0d298f4e9d41a06123a3c4e7443716
PopuloNes <metrolast.late2033@gmail.com>
2025-03-19 20:56:54 +0100

    Update mojaDrugaAplikacja.java

--- src/pl/pp/mojaDrugaAplikacja.java
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -1,36 +1,7 @@
 package pl.pp;
 
-import java.util.Scanner;
-
-public class mojaDrugaAplikacja {
+public class mojaPierwszaAplikacja {
     public static void main(String[] args) {
-        double x = 10; //utworzenie zmiennej typu double i przypisanie jej wartości 10
-        double y = 2;
-        Scanner scanner = new Scanner(System.in);
-        var result = x + y;
-        System.out.println("x + y = " + result);
-        result = x - y;
-        System.out.println("x - y = " + result);
-        result = x * y;
-        System.out.println("x * y = " + result);
-        result = x / y;
-        System.out.println("x / y = " + result);
-        result = x % y;
-        System.out.println("x % y = " + result);
-        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
-        double first = scanner.nextDouble(); //prośba o wpisanie wartości typu double
-        double second = scanner.nextDouble();
-        System.out.println("x + y = " + (first + second));
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Proszę podaj swoje imię:");
-//        String forename = scanner.nextLine();
-//
-//        System.out.println("Proszę podaj swoje imię:");
-//        String surname = scanner.nextLine();
-//
-//        scanner.close();
-//
-//        System.out.println("Witaj " + forename + " " + surname );
-
+        System.out.println("Witaj PP!");
     }
 }
