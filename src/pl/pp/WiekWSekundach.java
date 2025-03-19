d1d9daff951b22389dc1bcc41dde5ffde5469b08
Pohoreltsev <metrolast.late2033@gmail.com>
2025-03-19 21:14:55 +0100

    Lab2-2

--- src/pl/pp/WiekWSekundach.java
+++ src/pl/pp/WiekWSekundach.java
@@ -2,22 +2,22 @@ package pl.pp;
 
 import java.util.Scanner;
 
-public class WiekWSekundach {
+public class WiekWSekundach  {
     public static void main(String[] args) {
-        // Utworzenie skanera do odczytu danych od użytkownika
+
         Scanner scanner = new Scanner(System.in);
 
-        // Poproszenie o podanie wieku w latach
+
         System.out.print("Podaj swój wiek w latach: ");
         int wiekWLatach = scanner.nextInt();
 
-        // Przeliczenie wieku na sekundy
-        int wiekWSekundach = wiekWLatach * 365 * 24 * 60 * 60; // Zakładamy 365 dni w roku
 
-        // Wyświetlenie wyniku
+        int wiekWSekundach = wiekWLatach * 365 * 24 * 60 * 60;
+
+
         System.out.println("Twój wiek w sekundach to: " + wiekWSekundach);
 
-        // Zamknięcie skanera
+
         scanner.close();
     }
 }
