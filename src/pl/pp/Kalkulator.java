f54a04137b6f2f6d4568b173f5cfa4908a1e9056
Pohoreltsev <metrolast.late2033@gmail.com>
2025-03-19 21:30:56 +0100

    Kalkulator

---
+++ src/pl/pp/Kalkulator.java
@@ -0,0 +1,19 @@
+package pl.pp;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        // Tworzenie zmiennej całkowitej x i przypisanie jej wartości 10
+        int x = 10;
+
+        // Obliczenie dwukrotności liczby x
+        int dwukrotnoscX = x * 2;
+
+        // Obliczenie wartości x^2 (x do kwadratu)
+        int kwadratX = x * x;
+
+        // Wyświetlenie wszystkich wartości na konsoli
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność x = " + dwukrotnoscX);
+        System.out.println("x do kwadratu = " + kwadratX);
+    }
+}
