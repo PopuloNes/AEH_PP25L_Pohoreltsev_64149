184c144f721e4f7c84298bf27c312a22eca4b510
Pohoreltsev <metrolast.late2033@gmail.com>
2025-03-27 20:51:34 +0100

    lab4

--- src/pl/pp/SumOfSquares.java
+++ src/pl/pp/SumOfSquares.java
@@ -1,37 +1,27 @@
 package pl.pp;
 import java.util.Scanner;
 
-public class DaysToWeeksV2 {
+public class SumOfSquares {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int lowerLimit, upperLimit;
 
-        // First functionality: converting days to weeks and days
         while (true) {
-            System.out.print("Enter the number of days (or enter a non-positive value to complete this part): ");
-            int days = scanner.nextInt();
-            if (days <= 0) {
-                System.out.println("Let's move on to temperature conversion.");
+            System.out.print("Enter lower and upper integer limits: ");
+            lowerLimit = scanner.nextInt();
+            upperLimit = scanner.nextInt();
+
+            if (upperLimit <= lowerLimit) {
+                System.out.println("Done");
                 break;
             }
-            int weeks = days / 7;
-            int remainingDays = days % 7;
-            System.out.println(days + " days — is " + weeks + " weeks and " + remainingDays + " days.");
-        }
 
-        // Second functionality: temperature conversion
-        while (true) {
-            System.out.print("Enter temperature in degrees Fahrenheit (enter -1 to exit): ");
-            double fahrenheit = scanner.nextDouble();
-            if (fahrenheit == -1) {
-                System.out.println("The program is complete.");
-                break;
+            int sum = 0;
+            for (int i = lowerLimit; i <= upperLimit; i++) {
+                sum += i * i;
             }
-            // Calculate temperature in degrees Celsius
-            double celsius = (fahrenheit - 32) * 5 / 9;
-            // Calculate temperature in degrees Kelvin
-            double kelvin = celsius + 273.15;
-            // Output values accurate to two decimal places
-            System.out.printf("Fahrenheit: %.2f, Celsius: %.2f, Kelvin: %.2f%n", fahrenheit, celsius, kelvin);
+
+            System.out.printf("The sum of the squares from %d to %d is %d%n", lowerLimit * lowerLimit, upperLimit * upperLimit, sum);
         }
 
         scanner.close();
